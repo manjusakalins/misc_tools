ee21b845ac9290ec361b00813e2732f53ddf1765
linhuasheng <linhuasheng@jlinksz.com>
2016-11-21 10:16:06 +0800

    misc_for_rename_tar

---
+++ image_pack_unpack/sprd_rename.sh
@@ -0,0 +1,24 @@
+
+first_w="W"
+mv $first_w"P"* fdl1.bin
+mv $first_w"Q"* fdl2.bin
+mv $first_w"Z"* u-boot-spl-16k.bin
+mv $first_w"U"* u-boot.bin
+mv $first_w"B"* boot.img
+mv $first_w"H"* cache.img
+mv $first_w"S"* userdata.img
+mv $first_w"Y"* system.img
+mv $first_w"R"* recovery.img
+mv $first_w"O"* bootlogo.bmp
+mv $first_w"L"* fastbootlogo.bmp
+mv $first_w"C"* custom.img
+
+
+mv $first_w"N"* nvitem.bin
+mv $first_w"T"* nvitem_wcn.bin
+mv $first_w"E"* persist.img
+mv $first_w"V"* prodnv.img
+mv $first_w"K"* proinfo.img
+mv $first_w"J"* sysinfo.img
+
+
