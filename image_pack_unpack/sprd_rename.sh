d7d21e5961fb94ec32553eb0403b4ca879c1c1f6
linhuasheng <linhuasheng@jlinksz.com>
2017-03-06 15:23:47 +0800

    update_sprd_rename_file

--- image_pack_unpack/sprd_rename.sh
+++ image_pack_unpack/sprd_rename.sh
@@ -21,4 +21,14 @@ mv $first_w"V"* prodnv.img
 mv $first_w"K"* proinfo.img
 mv $first_w"J"* sysinfo.img
 
+mv $first_w"I"* LTE_DSP.bin
+mv $first_w"G"* SC9600_sharkls_3593_CUST_Base_NV_MIPI.dat
+mv $first_w"A"* SHARKL_DM_DSP.bin
+#mv $first_w"G"* SHARKL_GSMDSP_5M.bin
+mv $first_w"D"* PM_sharkls_arm7.bin
+mv $first_w"F"* wcn_fdl1.bin
+#mv $first_w"G"* SHARKL_CSFB_TDDSP.bin
+mv $first_w"W"* SC9600_sharkl_wphy_5mod_volte_zc.bin
+mv $first_w"M"* EXEC_KERNEL_IMAGE0.bin
+
 
